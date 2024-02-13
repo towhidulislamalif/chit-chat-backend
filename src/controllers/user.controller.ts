e9eab3e660424a5411caf8b6aee20fadeb701e5a
towhidulislamalif <towhidulislamalif@gmail.com>
2024-02-13 20:13:43 +0600

    Implement user login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import config from '../config';
 import APIError from '../error/APIError';
 import User from '../models/user.model';
 import { asyncRequestHandler } from '../utils/asyncRequestHandler';
@@ -48,5 +49,41 @@ export const registerUser = asyncRequestHandler(async (req, res) => {
     status: 201,
     message: 'User registration successful',
     data: newUser,
+  });
+});
+
+export const loginUser = asyncRequestHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validate required fields
+  if (!email || !password) {
+    throw new APIError(400, 'All fields are required');
+  }
+
+  // Check if the user already exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new APIError(404, 'User not found');
+  }
+
+  // compare the password
+  const checkPassword = await user.comparePassword(password);
+  if (!checkPassword) {
+    throw new APIError(400, 'Invalid password');
+  }
+
+  const access_token = user.generateAccessToken();
+  const refresh_token = user.generateRefreshToken();
+
+  res.cookie('refresh_token', refresh_token, {
+    httpOnly: true,
+    secure: config.node_env === 'production',
+  });
+
+  sendAPIResponse(res, {
+    success: true,
+    status: 200,
+    message: 'User login successfully',
+    data: { access_token },
   });
 });
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { registerUser } from '../controllers/user.controller';
+import { loginUser, registerUser } from '../controllers/user.controller';
 import { upload } from '../middleware/multer.middleware';
 
 const router = express.Router();
 
 // Route: POST /api/v1/auth/register
 router.post('/register', upload.single('avatar'), registerUser);
+
+// Route: POST /api/v1/auth/login
+router.post('/login', loginUser);
 
 export default router;
